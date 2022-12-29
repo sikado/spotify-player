cff352bb4d477c88f53196e5b9e3c7d6534d2783
Julien Brun <julien@julienbrun.fr>
2022-12-29 11:43:11 +0100

    feat(spotify-player): :sparkles: add 404 page

---
+++ apps/spotify-player/src/pages/404.tsx
@@ -0,0 +1,12 @@
+export default function Custom404() {
+  return (
+    <div
+      className="row align-items-center justify-content-center"
+      style={{ marginTop: 40 }}
+    >
+      <div className="col-auto">
+        <h1>404 - Page Not Found</h1>
+      </div>
+    </div>
+  );
+}
