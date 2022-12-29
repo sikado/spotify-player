1c1e0961cb2cc7c34a84ceba47889782cad0aa8f
Julien Brun <julien@julienbrun.fr>
2022-12-29 18:53:01 +0100

    ci(e2e): :wrench: extract baseURL from config

--- apps/spotify-player-e2e/src/smoketest.spec.js
+++ apps/spotify-player-e2e/src/smoketest.spec.js
@@ -1,13 +1,17 @@
+// @ts-check
 import { expect, test } from '@playwright/test';
+import { BASE_URL } from '../playwright.config';
+
+const url = new URL('/', BASE_URL);
 
 test.describe('Smoke tests for spotify-player', () => {
   test('open the main page', async ({ page }) => {
-    const response = await page.goto('/');
+    const response = await page.goto(url.href);
 
     // Test that the response did not fail
-    expect(response.status()).toBeLessThan(400)
+    expect(response.status()).toBeLessThan(400);
 
     // Take a screenshot
-    await page.screenshot({ path: 'playwright/test-results/screenshot.jpg' })
+    await page.screenshot({ path: 'playwright/test-results/screenshot.jpg' });
   });
 });
--- playwright.config.base.ts
+++ playwright.config.base.ts
@@ -1,12 +1,10 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
-const baseURL = process.env.E2E_BASE_URL || process.env.ENVIRONMENT_URL || 'http://localhost:4200/';
 
 export const baseConfig: PlaywrightTestConfig = {
   retries: 3,
   maxFailures: 2,
   timeout: 120000,
   use: {
-    baseURL,
   },
 };
