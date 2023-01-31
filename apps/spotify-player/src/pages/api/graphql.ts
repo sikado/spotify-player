7f788b75a95418758871e04790f80b611c90affa
Julien Brun <julien@julienbrun.fr>
2023-01-31 16:58:21 +0100

    perf(server): :lock: add cache & security to graphql

--- apps/spotify-player/src/pages/api/graphql.ts
+++ apps/spotify-player/src/pages/api/graphql.ts
@@ -1,3 +1,7 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { EnvelopArmorPlugin } from '@escape.tech/graphql-armor';
+import { useDisableIntrospection } from '@graphql-yoga/plugin-disable-introspection';
+import { useResponseCache } from '@graphql-yoga/plugin-response-cache';
 import { GRAPHQL_FILES } from '@spotify-player/core';
 import { SpotifyApiWrapper } from '@spotify-player/server-api';
 import { readFileSync } from 'fs';
@@ -11,6 +15,28 @@ export const config = {
   },
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let plugins: any[] = [
+  useResponseCache({
+    // global cache
+    session: () => null,
+  }),
+];
+let graphiql = true;
+
+if (process.env.NODE_ENV === 'production') {
+  plugins = [
+    ...plugins,
+    useDisableIntrospection(),
+    EnvelopArmorPlugin({
+      blockFieldSuggestion: {
+        enabled: true,
+      },
+    }),
+  ];
+  graphiql = false;
+}
+
 export default createYoga({
   schema: createSchema({
     typeDefs: readFileSync(join(GRAPHQL_FILES, 'schema.graphql'), 'utf-8'),
@@ -22,5 +48,6 @@ export default createYoga({
   }),
   // Needed to be defined explicitly because our endpoint lives at a different path other than `/graphql`
   graphqlEndpoint: '/api/graphql',
-  // plugins: [useDisableIntrospection()]
+  plugins,
+  graphiql,
 });
